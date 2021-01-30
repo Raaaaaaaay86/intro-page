1e556b7b6f79a34e2f5934c8d167196865f51428
Raaaaaaaay86 <mirror46258@gmail.com>
2021-01-30 11:36:32 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav>
-      <img :src="require('@/assets/imgs/logo.png')" height="50">
+      <img :src="require('@/assets/imgs/logo.png')" height="50" @click="scrollToSection('contact')">
       <ul class="navbar_nav">
         <li class="nav_items" @click="scrollToSection('about')">
           關於我
