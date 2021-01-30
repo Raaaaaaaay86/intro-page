7b5d586913d0d3405c5b855c2ffe6cdf27a7818c
Raaaaaaaay86 <mirror46258@gmail.com>
2021-01-30 13:47:17 +0800

    edit nuxtConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,16 +3,18 @@ export default {
   head: {
     title: 'intro-page',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.15.2/css/all.css', integrity: 'sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu', crossorigin: 'anonymous' },
+      {
+        rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.15.2/css/all.css', integrity: 'sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu', crossorigin: 'anonymous',
+      },
     ],
   },
 
@@ -27,10 +29,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {src: '@/plugins/smoothScroll', mode: 'client'},
+    { src: '@/plugins/smoothScroll', mode: 'client' },
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components: https://go.nuxtjs.dev/config-components>
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
